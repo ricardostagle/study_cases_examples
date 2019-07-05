5f9f6c788c693977b72f3b86dd324367991b2af6
Ricardo Sanchez <ricardo.stagle@gmail.com>
2019-07-05 15:29:57 -0500

    add new files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Study cases examples
 
 Try these examples going to https://npm.runkit.com/, you will see easy how is working this code.
+
+Sources:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Details_of_the_Object_Model
---
+++ git_basics.js
@@ -0,0 +1,20 @@
+
+//Copy to repository to your projects folder
+
+$ git clone https://github.com/youruser/yourrepository.git
+
+//Add a file to commit
+
+$ git add yourfile
+
+//Commit your change
+
+$ git commit -m "first commit text"
+
+//push your change to your repository
+
+$ git push -u origin master
+
+//In these cases, the "reset" command is your best friend:
+
+$ git reset --soft HEAD~1
